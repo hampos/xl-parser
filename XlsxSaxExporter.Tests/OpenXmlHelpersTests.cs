7874639beb6a4ff54b34f5196761f891b162c2b6
Theofanis Tokas <tokastheofanis@gmail.com>
2016-10-07 18:25:50 +0300

    Make internals visible to the tests assembly, start adding unit tests

---
+++ XlsxSaxExporter.Tests/OpenXmlHelpersTests.cs
@@ -0,0 +1,101 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace XlsxSaxExporter.Tests
+{
+    public class OpenXmlHelpersTests
+    {
+        [Fact]
+        public void Given_WorksheetPart_I_Can_Create_OpenXmlReader()
+        {
+            var temp = Path.GetTempFileName();
+            Create(temp);
+
+            using (var spreadsheetDocument = SpreadsheetDocument.Open(temp, false))
+            {
+                var worksheetPart = spreadsheetDocument.WorkbookPart.WorksheetParts.First();
+
+                var reader = OpenXmlHelpers.GetOpenXmlReader(worksheetPart);
+
+                reader.Should().NotBeNull();
+            }
+
+            File.Delete(temp);
+        }
+
+        [Fact]
+        public void Given_Xlsx_Path_I_Can_Get_Sheet_Dimensions()
+        {
+            var temp = Path.GetTempFileName();
+
+            var cell = new Cell()
+            {
+                CellValue = new CellValue("1")
+            };
+            var sheetData = new SheetData(new Row(cell));
+            
+            Create(temp, sheetDimensionRef: "A1:B2");
+
+            var dimensions = OpenXmlHelpers.GetDimensions(temp);
+
+            dimensions.Should().NotBeNull();
+            dimensions.MinRowNum.Should().Be(1);
+            dimensions.MaxRowNum.Should().Be(2);
+            dimensions.MinColNum.Should().Be(1);
+            dimensions.MaxColNum.Should().Be(2);
+
+            File.Delete(temp);
+        }
+
+        private void Create(string filepath, IEnumerable<Row> rows = null, string sheetDimensionRef = null)
+        {
+            // Create a spreadsheet document by supplying the filepath.
+            // By default, AutoSave = true, Editable = true, and Type = xlsx.
+            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.
+                Create(filepath, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+
+            // Add SheedData
+            SheetData sheetData = new SheetData();
+            if (rows != null)
+                sheetData.Append(rows);
+
+            worksheetPart.Worksheet = new Worksheet(sheetData);
+
+            // Add a SheetDimension
+            SheetDimension sheetDimension = new SheetDimension() { Reference = sheetDimensionRef };
+            worksheetPart.Worksheet.Append(sheetDimension);
+
+            // Add Sheets to the Workbook.
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild<Sheets>(new Sheets());
+
+            // Append a new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+            sheets.Append(sheet);
+
+            workbookpart.Workbook.Save();
+
+            // Close the document.
+            spreadsheetDocument.Close();
+        }
+    }
+}
--- XlsxSaxExporter.Tests/Properties/AssemblyInfo.cs
+++ XlsxSaxExporter.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XlsxSaxExporter")]
+[assembly: AssemblyTitle("XlsxSaxExporter.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XlsxSaxExporter")]
+[assembly: AssemblyProduct("XlsxSaxExporter.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3e56319a-e634-4e03-8a96-a608b8ff83f2")]
+[assembly: Guid("9d9a45e3-0c95-408e-bf62-d3b7e3494546")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("XlsxSaxExporter.Tests")]
