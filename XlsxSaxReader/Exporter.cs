aaf5ba7976f66dc6888fb8e0bc663a2aa69ea354
Theofanis Tokas <tokastheofanis@gmail.com>
2016-10-07 16:33:25 +0300

    Add Exporter class with static method Export to offer full export functionality with one call

---
+++ XlsxSaxReader/Exporter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace XlsxSaxReader
+{
+    public class Exporter
+    {
+        public static List<List<string>> Export(string path, int pageSize)
+        {
+            IXlsxSaxReader xlsxSaxReader = new XlsxSaxReader(path, pageSize);
+
+            int page = 1;
+            var rows = new List<List<string>>(xlsxSaxReader.Dimensions.MaxRowNum);
+
+            do
+            {
+                var result = xlsxSaxReader.Read(page++);
+                if (result.Count == 0)
+                    break;
+
+                rows.AddRange(result);
+            } while (true);
+
+            return rows;
+        }
+    }
+}
